2c72ed678cb98aed63ebd50bb701d9f1178452ad
david felipe <davidfelipe_306@hotmail.com>
2020-11-08 01:14:45 -0400

    Adicionando methods update e delete rest api

--- src/main/java/com/contact/controllers/ContactController.java
+++ src/main/java/com/contact/controllers/ContactController.java
@@ -5,19 +5,24 @@ import com.contact.models.Contato;
 import com.contact.presenters.ContactPresenter;
 import com.contact.repository.ContactRepository;
 import com.contact.services.ContactStoreService;
+import com.contact.services.ContactUpdateService;
 import com.contact.validaterequest.ContactFormValidate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/contact")
 public class ContactController {
     @Autowired
     ContactStoreService contactService;
+    @Autowired
+    ContactUpdateService contactUpdateService;
     @Autowired
     ContactRepository contactRepository;
     @Autowired
@@ -34,5 +39,33 @@ public class ContactController {
     public ContactPresenter store(@Valid @RequestBody ContactFormValidate request) {
         Contato contato = contactConverter.toEntity(request);
         return contactConverter.toModel(contactService.store(contato));
+    }
+
+    @GetMapping("/contatoId")
+    public ResponseEntity<ContactPresenter> filter(@PathVariable Long contatoId) {
+        Optional<Contato> contato = contactRepository.findById(contatoId);
+        if(contato.isPresent()) {
+            ContactPresenter present = contactConverter.toModel(contato.get());
+            return ResponseEntity.ok(present);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @PutMapping("/{contatoId}")
+    public ResponseEntity<Contato> update(@Valid @PathVariable Long contatoId,
+                                          @RequestBody Contato contato) {
+        if(!contactRepository.existsById(contatoId)) {
+            return ResponseEntity.notFound().build();
+        }
+        contactUpdateService.update(contatoId, contato);
+        return ResponseEntity.ok(contato);
+    }
+    
+    @DeleteMapping("/{contatoId}")
+    public ResponseEntity<Void> delete(@PathVariable Long contatoId) {
+        if(!contactRepository.existsById(contatoId)) {
+            return ResponseEntity.notFound().build();
+        }
+        contactRepository.deleteById(contatoId);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/contact/services/ContactUpdateService.java
+++ src/main/java/com/contact/services/ContactUpdateService.java
@@ -1,2 +1,17 @@
-package com.contact.services;public class ContactUpdateService {
+package com.contact.services;
+
+import com.contact.models.Contato;
+import com.contact.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactUpdateService {
+    @Autowired
+    ContactRepository contactRepository;
+
+    public Contato update(Long contatoId, Contato contato) {
+        contato.setId(contatoId);;
+        return contactRepository.save(contato);
+    }
 }
