54a4e6c6622a67c78851356e453dd80443d9db08
David Felipe Santos Oliveira <davidfelipe_306@hotmail.com>
2020-11-11 09:26:42 -0400

    adicionando script sql

--- database/scriptsqldb.sql
+++ database/scriptsqldb.sql
@@ -5,10 +5,10 @@ use contacts;
 CREATE TABLE contatos (
   id bigint(20) NOT NULL AUTO_INCREMENT,
   created_date datetime DEFAULT NULL,
-  email varchar(255) DEFAULT NOT NULL,
-  nome varchar(255) DEFAULT NOT NULL,
-  sexo varchar(255) DEFAULT NOT NULL,
-  telefone varchar(255) DEFAULT NOT NULL,
+  email varchar(255) NOT NULL,
+  nome varchar(255)  NOT NULL,
+  sexo char(1)  NOT NULL,
+  telefone varchar(255)  NOT NULL,
   updated_date datetime DEFAULT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
